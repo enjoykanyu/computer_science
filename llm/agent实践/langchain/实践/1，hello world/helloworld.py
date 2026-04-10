90b5aa6aec2635667b8809f4b5fa376201b65906
enjoykanyu <creatkanyu@outlook.com>
2026-04-10 23:15:33 +0800

    chore:langchain实践修改模型

--- llm/agent实践/langchain/实践/1，hello world/helloworld.py
+++ llm/agent实践/langchain/实践/1，hello world/helloworld.py
@@ -5,7 +5,7 @@ def get_weather(city: str) -> str:
     return f"It's always sunny in {city}!"
 
 agent = create_agent(
-    model="ollama:qwen3:0.6b",
+    model="ollama:llama3.2:3b",
     tools=[get_weather],
     system_prompt="You are a helpful assistant",
 )
